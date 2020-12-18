aa09e918cd81c2658a4d9781cf6f0cb099bc8c24
bhoomika369 <73927720+bhoomika369@users.noreply.github.com>
2020-12-18 21:33:19 +0530

    Add files via upload

---
+++ Services.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../../App.css';
+export default function Services() {
+    <p> we offer worldclass Services</p>
+
+  return <h1 className='services'>SERVICES</h1>;
+
+}
